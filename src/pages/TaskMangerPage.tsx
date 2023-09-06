ee57cd7f3f20e2b5cd8757ed8b87519635f359e9
Olesia Polevoi <olesia.polevoi@gmail.com>
2023-09-06 18:06:28 -0400

    added state to manage tasks and rearranged components

--- src/pages/TaskMangerPage.tsx
+++ src/pages/TaskMangerPage.tsx
@@ -1,13 +1,47 @@
+import { useState } from "react";
 import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { TaskList } from "../components/TaskList/TaskList";
+import { TaskItem } from "../types/task";
 
 export const TaskManagerPage = () => {
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+  const createNewTask = (text: string) => {
+    const newTask: TaskItem = {
+      id: tasks.length,
+      text: text,
+      isDone: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
+  const updateTaskStatus = (task: TaskItem) => {
+    const updatedTasks = tasks.map((el) => {
+      if (el.id === task.id) {
+        el.isDone = !el.isDone;
+      }
+      return el;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (id: number) => {
+    const filteredTasks = tasks.filter((el) => {
+      return el.id !== id;
+    });
+    setTasks(filteredTasks);
+  };
+
   return (
     <>
       <Header />
-      <Form />
-      <TaskList />
+      <Form createNewTask={createNewTask} />
+      <TaskList
+        tasks={tasks}
+        updateTaskStatus={updateTaskStatus}
+        deleteTask={deleteTask}
+      />
     </>
   );
 };
